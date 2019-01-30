8185eca899c0d81a1c892b99a5c278e2a03361b7
Charles Jaimet <charles@calmseamedia.com>
2019-01-30 14:58:58 -0500

    Add some validation to Fizz Buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,7 @@
-def fizzbuzz( min, max, opts = {} )
-  ( min..max ).each do |num|
+def fizzbuzz( min = 0, max = 100, opts = {} )
+  range = validate_range( min, max )
+  opts  = validate_opts( opts )
+  range.each do |num|
     out = num.to_s
     opts.keys.each do |key|
       if 0 == num % key
@@ -10,8 +12,54 @@ def fizzbuzz( min, max, opts = {} )
   end
 end
 
-fizzbuzz( 3, 30, {
+def validate_range( min, max )
+  min = min.to_i
+  max = max.to_i
+  if min >= max
+    max = min + 1
+  end
+  range = min..max
+  return range
+end
+
+def validate_opts( opts )
+  def_opts = {
+    2 => 'fizz',
+    3 => 'buzz'
+  }
+  if ! opts.is_a?( Hash )
+    return def_opts
+  end
+  opts.delete_if{ | key, val| ! key.is_a?( Integer ) }
+  if opts.empty?
+    return def_opts
+  end
+  return opts
+end
+
+
+
+
+fizzbuzz( -10, 10, {
   2 => 'fizz',
   3 => 'buzz',
   5 => 'splat'
+} )
+
+puts ''
+
+fizzbuzz( 3, 15, 'dog' )
+
+puts ''
+
+fizzbuzz( -10, 10, {
+  2 => 'fizz',
+  'dog' => 'buzz',
+  5 => 'splat'
+} )
+
+puts ''
+
+fizzbuzz( 0, 10, {
+  'dog' => 'buzz'
 } )
